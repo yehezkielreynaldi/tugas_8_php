f7746af0357fc784191f3f579dadddbf9ef48394
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2022-03-25 17:24:13 +0700

    Tugas 8 PHP
    Tugas ini saya kirimkan untuk Team Niomic,Terimakasih.

---
+++ tugas8.php
@@ -0,0 +1,76 @@
+<?php
+
+for ($a = 10; $a > 0; $a--) {
+    for ($i = 1; $i <= $a; $i++) {
+        echo "_";
+    }
+    for ($a1 = 10; $a1 > $a; $a1--) {
+        echo "x";
+    }
+    for ($a2 = 10; $a2 > $a; $a2--) {
+        echo "x";
+    }
+    echo "<br>";
+}
+for ($b = 0; $b <= 10; $b++) {
+    for ($j = 1; $j <= $b; $j++) {
+        echo "_";
+    }
+    for ($b1 = 10; $b1 > $b; $b1--) {
+        echo "x";
+    }
+    for ($b2 = 10; $b2 > $b; $b2--) {
+        echo "x";
+    }
+    echo "<br>";
+}
+
+for ($c = 10; $c > 0; $c--) {
+    for ($k = 1; $k <= $c; $k++) {
+        echo "_";
+    }
+    for ($c1 = 10; $c1 > $c; $c1--) {
+        echo "x";
+    }
+    for ($c2 = 10; $c2 > $c; $c2--) {
+        echo "x";
+    }
+    echo "<br>";
+}
+for ($d = 0; $d <= 10; $d++) {
+    for ($l = 1; $l <= $d; $l++) {
+        echo "_";
+    }
+    for ($d1 = 10; $d1 > $d; $d1--) {
+        echo "x";
+    }
+    for ($d2 = 10; $d2 > $d; $d2--) {
+        echo "x";
+    }
+    echo "<br>";
+}
+
+for ($e = 10; $e > 0; $e--) {
+    for ($l = 1; $l <= $e; $l++) {
+        echo "_";
+    }
+    for ($e1 = 10; $e1 > $e; $e1--) {
+        echo "x";
+    }
+    for ($e2 = 10; $e2 > $e; $e2--) {
+        echo "x";
+    }
+    echo "<br>";
+}
+for ($f = 0; $f <= 10; $f++) {
+    for ($m = 1; $m <= $f; $m++) {
+        echo "_";
+    }
+    for ($f1 = 10; $f1 > $f; $f1--) {
+        echo "x";
+    }
+    for ($f2 = 10; $f2 > $f; $f2--) {
+        echo "x";
+    }
+    echo "<br>";
+}
